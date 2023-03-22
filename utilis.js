519eb657ee0664e0f7ccc211a6d5bf9bb18edfa3
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-22 12:10:58 +0200

    Create utilis.js

---
+++ utilis.js
@@ -0,0 +1,28 @@
+function getElement(id) {
+    return document.getElementById(id)
+}
+
+function makeAPICall (url, options) {
+    return fetch(url, options)
+    .then(response => {
+        if(response.ok) {
+            return response.json()
+        }
+
+        return new Promise ((resolve, reject) => {
+            response.json().then(body => {
+            reject(new Error(body.message))
+        })
+    })
+        
+        
+    })
+}
+
+function debounce (action,second) {
+    let timerId = null;
+    return function(...event){
+        clearTimeout(timerId);
+        timerId = setTimeout(action, second, ...event);
+    }
+}
