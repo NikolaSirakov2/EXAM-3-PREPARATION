68dc12d0534482b5506464be9c7f28aa25a6de99
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-22 21:47:25 +0200

    Update DetailsController.js

--- model/DetailsController.js
+++ model/DetailsController.js
@@ -8,24 +8,27 @@ class DetailsController {
       let detailsContainer = getElement("detailsContainer");
       detailsContainer.innerHTML = "";
   
-        this.partiesManager.getAll().then((data) => {
-        this.renderDetailsContainer(data, detailsContainer);
+       
+
+        this.partiesManager.getAll()
+        .then((data) => {
+        this.renderDetailsContainer(data[1].picture, data[1].slogan , detailsContainer);
       });
     };
   
   
-    renderDetailsContainer = (list, container) => {
+    renderDetailsContainer = (picture, slogan, container) => {
        
         let card = document.createElement("div");
         card.classList.add("detailsCard");
         
-        card.innerHTML = `
-                            <img src="${list[0].picture}" class="card-img-top" alt="...">
-                            <p class="card-text">${list[0].slogan}</p>
-                            
+        card.innerHTML = `  <span>
+                            <img src="${picture}" class="card-img-top" alt="...">
+                            <h1 class="card-text">${slogan}</h1>
+                            </span>
             <div class="card-body">
               <h1 class="card-title">Leader names</h1>
-              <h5>Всички знайни и незнайни войни загинали в борбата за свобода и справедливост</h5>
+              <h5>Всички знайни и незнайни воини загинали в борбата за свобода и справедливост</h5>
               <a href="#voteForUs" class="btn btn-primary">vote for us</a>
               <a href="#results" class="btn btn-primary">view results</a>
             </div>`;
