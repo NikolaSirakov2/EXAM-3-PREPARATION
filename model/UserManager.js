3ca59b71bae9bbb38706cc12e974c82af6761c82
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-22 12:10:55 +0200

    Create UserManager.js

---
+++ model/UserManager.js
@@ -0,0 +1,39 @@
+class UserManager {
+
+    loggedUser = null;
+
+    constructor(){
+        this.loggedUser 
+    }
+
+
+    register = (username, password) => {
+        return makeAPICall(SERVER_URL + '/users', {
+            method: "POST",
+            headers: {
+                "Content-type" : "application/json"
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        })
+        
+    }
+
+    logIn = (username, password) => {
+        return makeAPICall(SERVER_URL + '/login', {
+            method: "POST",
+            headers: {
+                "Content-type" : "application/json"
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        })
+        .then(({hasVoted, sessionId, username}) => {
+                localStorage.setItem('loggedUser', JSON.stringify({hasVoted, sessionId, username}))
+        })
+    }
+}
