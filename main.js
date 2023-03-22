b610fa3cb1aade5cb6832c6b399bfe95d6bc67e6
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-22 12:10:43 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,44 @@
+class ViewController {
+    constructor() {
+        window.addEventListener("load", this.handleHashChange);
+        window.addEventListener("hashchange", this.handleHashChange);
+        this.userManager = new UserManager();
+        this.partiesManager = new PartiesManager();
+
+        this.registerController = new RegisterController(this.userManager);
+        this.logInController = new LogInController(this.userManager);
+        this.listingController = new ListingController(this.partiesManager);
+   }
+
+    handleHashChange = (e) => {
+        const hash = location.hash.slice(1) || PAGE_IDS[0];
+
+        if(!PAGE_IDS.includes(hash)){
+            location.hash = "error";
+            return;
+        }
+
+
+        PAGE_IDS.forEach(pageId => {
+            let element = getElement(pageId);
+            if (hash === pageId) {
+                element.style.display = 'block' 
+            } else { 
+                element.style.display = "none";
+            }
+        })
+
+        switch(hash) {
+            case 'register':
+                this.registerController.render();
+                break;
+            case 'login':
+                this.logInController.render();
+                break; 
+            case 'listings':
+                this.listingController.render();  
+        }
+    }
+}
+
+let viewController = new ViewController();
