71d3ea7b452f6f20019e60ccdd536b4eaf23604d
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-23 17:30:39 +0200

    Update DetailsController.js

--- model/DetailsController.js
+++ model/DetailsController.js
@@ -29,9 +29,24 @@ class DetailsController {
             <div class="card-body">
               <h1 class="card-title">Leader names</h1>
               <h5>Всички знайни и незнайни воини загинали в борбата за свобода и справедливост</h5>
-              <a href="#voteForUs" class="btn btn-primary">vote for us</a>
               <a href="#results" class="btn btn-primary">view results</a>
             </div>`;
+
+            let  vote = createElement("button");
+            vote.id = "voteForUs"
+            vote.innerText = "vote for us";
+            
+            
+            vote.addEventListener("click", (e) => {
+              let vote = JSON.parse(localStorage.loggedUser);
+              if(vote.hasVoted === true){
+                  alert("You already voted!")
+              } else {
+              vote.hasVoted = true;
+              localStorage.setItem('loggedUser', JSON.stringify(vote))
+            }})
+        
+            card.append(vote)
     
         container.appendChild(card);
       
