7a21b2e4eaec5727bed9eaff623633770ac198e5
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-25 15:27:30 +0200

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-const PAGE_IDS = ["login", "results", "register", "listings", "details", "error"];
+const PAGE_IDS = ["register", "login", "results", "listings", "details", "error"];
 const SERVER_URL = 'http://localhost:8080';
