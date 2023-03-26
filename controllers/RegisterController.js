c723727e4154f0cf0356091a0ce93b3d961f2494
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-26 11:27:34 +0300

    Create RegisterController.js

---
+++ controllers/RegisterController.js
@@ -0,0 +1,39 @@
+class RegisterController {
+
+    constructor(userManager) {
+        this.userManager = userManager;
+    }
+
+    render = () => {
+        let link = getEl("link")
+        let headerText = getEl("headerText")
+        link.innerText = "";
+        headerText.innerText = "";
+        let form = getEl("regForm");
+        let userManag = getEl("userManag");
+
+    
+        userManag.innerText = "LogIn";
+        userManag.href = "#login";
+
+        form.onsubmit = (event) => {
+                event.preventDefault();
+
+                const {username: {value: username}, 
+                    password: {value: password}, 
+                    confirmPass: {value: confirmPass}} = event.currentTarget;
+
+                console.log(username, password, confirmPass);
+
+                this.userManager.register(username, password)
+                .then(data => {
+                    location.hash = 'login';
+                })
+                .catch(error => {
+                    alert(error)
+                })
+
+                form.reset();
+        }
+    }
+}
