d41c1a01b9770b74f6dbf1df067eb22da937f57d
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-26 11:27:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,52 @@
 class ViewController {
-    constructor() {
-        window.addEventListener("load", this.handleHashChange);
-        window.addEventListener("hashchange", this.handleHashChange);
-        this.userManager = new UserManager();
-        this.partiesManager = new PartiesManager();
+  constructor() {
+    window.addEventListener("load", this.handleHashChange);
+    window.addEventListener("hashchange", this.handleHashChange);
+    this.userManager = new UserManager();
+    this.partiesManager = new PartiesManager();
+    this.detailsManager = new DetailsManager();
+    this.resultsManager = new ResultsManager(this.detailsManager);
 
-        this.registerController = new RegisterController(this.userManager);
-        this.logInController = new LogInController(this.userManager);
-        this.listingController = new ListingController(this.partiesManager);
-        this.detailsController = new DetailsController(this.partiesManager);
-        this.resultsController = new ResultsController();
-   }
+    this.resultsController = new ResultsController(this.resultsManager);
+    this.registerController = new RegisterController(this.userManager);
+    this.logInController = new LogInController(this.userManager);
+    this.detailsController = new DetailsController(this.userManager, this.resultsManager, this.resultsController);
+    this.listingController = new ListingController(this.partiesManager, this.detailsManager, this.detailsController, this.userManager);
+  }
 
-    handleHashChange = (e) => {
+  handleHashChange = (e) => {
+    const hash = location.hash.slice(1) || PAGE_IDS[1];
 
-        
-        const hash = location.hash.slice(1) || PAGE_IDS[1];
-
-        if(!PAGE_IDS.includes(hash)){
-            location.hash = "error";
-            return;
-        }
-
-
-        PAGE_IDS.forEach(pageId => {
-            let element = getElement(pageId);
-            if (hash === pageId) {
-                element.style.display = 'block' 
-            } else { 
-                element.style.display = "none";
-            }
-        })
-
-        switch(hash) {
-            case 'register':
-                this.registerController.render();
-                break;
-            case 'login':
-                localStorage.loggedUser = [];
-                this.logInController.render();
-                break; 
-            case 'listings':
-                this.listingController.render();
-            case 'details' :
-                this.detailsController.render();
-            case 'results' :
-                this.resultsController.render();    
-        }
+    if (!PAGE_IDS.includes(hash)) {
+      location.hash = "error";
+      return;
     }
+
+    PAGE_IDS.forEach((pageId) => {
+      let element = getEl(pageId);
+      if (hash === pageId) {
+        element.style.display = "block";
+      } else {
+        element.style.display = "none";
+      }
+    });
+
+    switch (hash) {
+      case "register":
+        this.registerController.render();
+        break;
+      case "login":
+        localStorage.loggedUser = [];
+        this.logInController.render();
+        break;
+      case "listings":
+        this.listingController.render();
+      case "details":
+        this.detailsController.render();
+      case "results":
+        this.resultsController.render();
+    }
+  };
 }
 
 let viewController = new ViewController();
