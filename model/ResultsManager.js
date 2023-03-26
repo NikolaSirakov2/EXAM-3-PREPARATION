734027fc90402c087ca44875c675977e607a796c
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-26 14:54:08 +0300

    Update ResultsManager.js

--- model/ResultsManager.js
+++ model/ResultsManager.js
@@ -4,15 +4,56 @@ class ResultsManager {
         this.detailsManager = detailsManager;
     }
 
+
     getResults = () => {
 
         let sessionId = JSON.parse(localStorage.getItem('loggedUser')).sessionId;
+
+        let overallResults = null;
 
         return makeAPICall(SERVER_URL + "/results", {
             headers: {
                 "identity" : sessionId,
             }
-        
+        })
+        .then(results => {
+                let allVoters = results.reduce((acc, parties) => {
+                    return acc + parties.voters;
+                }, 0)
+
+                return results.map(party => {
+
+                    party.voters = Number((party.voters / allVoters * 100).toFixed(2));
+                    return party;
+
+                }).sort((a, b) => {
+                    return b.voters - a.voters;
+                })
+        })
+        .then(overall => {
+            
+            overallResults = overall;
+
+            let partyArr = [];
+
+            overall.forEach(party => {
+
+                partyArr.push(this.detailsManager.getDetails(party.partyId));
+
+            });
+
+            return Promise.all(partyArr)
+        })
+        .then(res => {
+
+            console.log(res);
+
+            for (let i = 0; i < res.length; i++) {
+
+                overallResults[i].partyId = res[i].name
+            }
+
+            return overallResults;
         })
     }
 }
