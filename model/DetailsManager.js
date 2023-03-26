cca53131ddc47b9fad7ce1bbf7300fb9e34e6ff5
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-26 11:27:46 +0300

    Create DetailsManager.js

---
+++ model/DetailsManager.js
@@ -0,0 +1,14 @@
+class DetailsManager {
+
+    getDetails = (partyId) => {
+
+        const sessionId = JSON.parse(localStorage.getItem('loggedUser')).sessionId;
+
+        return makeAPICall(SERVER_URL + `/party/${partyId}`,{
+            headers: {
+                'identity': sessionId
+            }
+        })
+    }
+
+}
